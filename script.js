3817a34e2af92623d0d8d0bdf46e0550d3de1f90
jungloul1 <jungloul1@gmail.com>
2021-07-03 17:47:47 +0100

    modal v1

---
+++ script.js
@@ -0,0 +1,26 @@
+"use strict";
+const modal = document.querySelector(".modal");
+const overlay = document.querySelector(".overlay");
+const btnCloseModal = document.querySelector(".close-modal");
+const titleModal = document.querySelector(".titleModal");
+const btnOpenModal = document.querySelectorAll(".show-modal");
+
+const closeModal = function () {
+  modal.classList.add("hidden");
+  overlay.classList.add("hidden");
+};
+
+for (let i = 0; i < btnOpenModal.length; i++)
+  btnOpenModal[i].addEventListener("click", () => {
+    titleModal.textContent = `I'm the modal number ${i + 1} 😍`;
+    modal.classList.remove("hidden");
+    overlay.classList.remove("hidden");
+  });
+
+btnCloseModal.addEventListener("click", closeModal);
+overlay.addEventListener("click", closeModal);
+document.addEventListener("keydown", (e) => {
+  if (e.key === "Escape" && !modal.classList.contains(".hidden")) {
+    closeModal();
+  }
+});
